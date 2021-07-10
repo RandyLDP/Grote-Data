b73e00d140bd52d5be613f8a186f9dcecdbe1395
Randy Duran <randyduranperez@gmail.com>
2021-07-11 00:13:08 +0200

    vrouwen array doet het niet

--- script.js
+++ script.js
@@ -13,29 +13,44 @@ const getLanden = landen => {
     })}
  landen.addEventListener('click', getLanden)
 
- const getVrouwen =  () => {
-    let steenbok = randomPersonData.filter( e =>{
-        let month = e.birthday.dmy.slice(3, 5);
-        let day = e.birthday.dmy.slice(0, 2);
-        if (month === "12" || month === "01") {
-        return ( e.gender === "female" && e.age > 30 &&
-        day >= 19 && day <= 22 )
-    }
-  })}
+
+ const isWoman = person => person.gender === "female";
+
+ const isOver30 = person => person.age > 30;
  
-    let persons = steenbok.map( e => {
-    return e.name + " " + e.surname + " " + e.photo;
-  });
-  
-    let sortedPersons = persons.sort();
-  
-    sortedPersons.forEach(e => {
-        let newLi = document.createElement("li");
+ const isCapricorn = person => person.sign === CAPRICORN;
+ 
+ const sortByName = (person1, person2) =>
+   sort_helper(person1.name > person2.name);
+ 
+ const getCapricornWomen = personData =>
+   personData.filter(isWoman).filter(isOver30).map(addStarSign).filter(isCapricorn).sort(sortByName);
+ 
+ const generateCapricornWomanHTML = ({name,surname, photo, age, birthday }) => {
    
-    document.querySelector("#list").appendChild(newLi);
-    newLi.textContent = e;
-  });
-
-  vrouwen.addEventListener('click', getVrouwen)
-
-
+   const nameSpan = document.createElement("span");
+   nameSpan.innerHTML = `${name} ${surname}`;
+ 
+   const photoSpan = document.createElement("span");
+   photoSpan.innerHTML = photo;
+ 
+   const ageSpan = document.createElement("span");
+   ageSpan.innerHTML = age;
+ 
+   const birthdaySpan = document.createElement("span");
+   birthdaySpan.innerHTML = birthday.dmy;
+ 
+   const li = document.createElement("li");
+   li.appendChild(nameSpan);
+   li.appendChild(photoSpan);
+   li.appendChild(ageSpan);
+   li.appendChild(birthdaySpan)
+ 
+   return li;
+ };
+ 
+ const displayCapricornWomen = () => {
+   getCapricornWomen.map(generateCapricornWomanHTML).forEach(addToResultList);
+ };
+ 
+ document.querySelector("#list").addEventListener("click", displayCapricornWomen);
